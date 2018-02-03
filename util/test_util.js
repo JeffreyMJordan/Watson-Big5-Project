cd7c64cde8abf58795a1c1bba430159f0950de25
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2018-02-03 12:10:41 -0800

    Initial commit

---
+++ util/test_util.js
@@ -0,0 +1,10 @@
+
+//Example version: 2017-10-13
+export const getBigFive = (content, version) => {
+  return $.ajax({
+    url: '',
+    method: 'POST',
+    data: {content: content, 
+           version: version}
+  }).then(res => console.log(res));
+};
