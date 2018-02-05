1e4230195aea562414f700d5105bb12dc7481e25
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2018-02-04 17:34:05 -0800

    Updates express server

--- server.js
+++ server.js
@@ -1,6 +1,41 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+var watson = require('watson-developer-cloud');
+var credentials = require('./credentials');
+// bundle the code
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpack = require('webpack');
+var webpackConfig = require('./webpack.config');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var compiler = webpack(webpackConfig);
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.use(
+  webpackDevMiddleware(compiler, {
+    publicPath: '/' 
+  })
+);
+
+app.use(express.static('public/'));
+
+// For local development, specify the username and password or set env properties
+var ltAuthService = new watson.AuthorizationV1({
+  username: credentials.credentials.username,
+  password: credentials.credentials.password,
+  url: watson.PersonalityInsightsV3.URL
+});
+
+app.get('/api/token/personality_insight', function(req, res) {
+  ltAuthService.getToken(function(err, token) {
+    if (err) {
+      console.log('Error retrieving token: ', err);
+      return res.status(500).send('Error retrieving token');
+    }
+    res.send(token);
+  });
+});
+
+var port = 3000;
+
+app.listen(port, function() {
+  console.log('Watson browserify example server running at http://localhost:%s/', port);
+});
